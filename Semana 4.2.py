2f555787ac6b20132020cdfd50c31fb61719f0e7
bpla17 <brunoplahdz@gmail.com>
2025-11-06 08:52:35 -0600

    Add files via upload

---
+++ Semana 4.2.py
@@ -0,0 +1,19 @@
+# Ejercicio 7
+print("\nEjercicio 7 - Loop infinito")
+# Queremos contar del 1 al 5
+contador = 1
+while contador <= 5:
+ print(f"Número: (contador]")
+print("Aqui terminan los ejercicios de while")
+
+
+
+# Ejercicio 8
+print("\nEjercicio & - Loop corregido")
+# Queremos contar del 1 al 5
+contador = 1
+while contador <= 5:
+ print(f"Número: (contador)")
+contador = contador + 1 # ¡Ahora sí incremetamos!
+
+print("¡Loop terminado correctamente/\n") 
